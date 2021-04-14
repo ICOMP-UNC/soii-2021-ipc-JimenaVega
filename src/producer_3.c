63adb8dbd4649d04f14cf0fd849f606b788e505f
JimenaVega <jimevega28@gmail.com>
2021-04-14 11:52:44 -0300

    finished dommies (uncommunicated) producers

--- src/producer_3.c
+++ src/producer_3.c
@@ -2,12 +2,26 @@
 #include <stdio.h>
 #include <string.h>
 
+#define Q_NORM_LOAD 3
 
 int get_CPUs();
+void get_load(float load_avg[Q_NORM_LOAD]);
 
 int main(){
 
-    printf("CPUs = %d", get_CPUs());
+    float load_avg[Q_NORM_LOAD];
+    int norm_load[Q_NORM_LOAD];
+    float n_cpus = (float)get_CPUs();
+    get_load(load_avg);
+
+    for(int i=0; i<Q_NORM_LOAD; i++){
+        norm_load[i] = (int)((load_avg[i]/n_cpus)*100);
+    }
+
+    for(int i=0; i<Q_NORM_LOAD;i++){
+        printf("norm_load [%d] = %d%%\n",i,norm_load[i]);
+    }
+
     return 0;
 }
 
@@ -31,4 +45,17 @@ int get_CPUs(){
     fclose(file);
     
     return CPU_quant;
+}
+
+void get_load(float load_avg[Q_NORM_LOAD]){
+
+    FILE *file = fopen("/proc/loadavg", "r");
+    char buffer[100];
+
+    fgets(buffer,100,file);
+    load_avg[0] = strtof(strtok(buffer, " "),NULL);
+
+    for(int i = 1; i < Q_NORM_LOAD; i++){
+        load_avg[i] = strtof(strtok(NULL, " "),NULL);    
+    }
 }
